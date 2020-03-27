d2f933e1ad4e24ef0d8cce4703ae6f7ea5464acd
Kevin <kevin.m.guertin@gmail.com>
2020-03-27 17:06:17 -0400

    add next group of exercises

---
+++ ex8.rb
@@ -0,0 +1,16 @@
+print "Give me a number: "
+number = gets.chomp.to_i
+
+bigger = number * 100
+puts "A bigger number is #{bigger}."
+
+print "Give me another number: "
+another = gets.chomp
+number = another.to_i
+
+smaller = number / 100
+puts "A smaller number is #{smaller}."
+
+print "Enter a dollar amount: "
+dollars = gets.chomp.to_f
+puts "10% is #{dollars * 0.10}"
